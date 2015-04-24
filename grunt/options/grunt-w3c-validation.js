6f52429ab94ec17a4dacdb6634e229569540537c
Vikash Bhardwaj <vikasbhardwaj7@gmail.com>
2015-04-25 00:19:46 +0530

    Updated Package.json as well as validation template and also added local html folder for local URLs testing.

--- grunt/options/grunt-w3c-validation.js
+++ grunt/options/grunt-w3c-validation.js
@@ -16,17 +16,18 @@ module.exports = function( grunt ) {
             stoponerror: false, //If this is set to true, validator will stop validating next file
             remotePath: "http://www.w3schools.com/",
             remoteFiles: "w3c_validation_urls.json", // JSON file contains array of page paths.
+            //wrapfile:'local_html_test/wrapper.html',
             relaxerror: ['& did not start a character reference. (& probably should have been escaped as &amp;.)',
                     'A meta element with an http-equiv attribute whose value is X-UA-Compatible must have a content attribute with the value IE=edge.',
                     'Attribute xmlns:fb not allowed here.', 'Attribute with the local name xmlns:fb is not serializable as XML 1.0.'], // We can specify here the list of errors which we want to ignore
             
             generateReport: true,
             errorHTMLRootDir: "w3cErrorFolder",
-            useTimeStamp: true,
+            useTimeStamp: false,
             errorTemplate: "w3c_validation_error_Template.html"
         },
         files: {
-            src: []
+            src: ['local_html_test/*.html']
         }
     });
 };
